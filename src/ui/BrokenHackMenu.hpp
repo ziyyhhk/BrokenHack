badf05996922d8885cb9e6743f41acc1d86cd835
Akira <akiranshisthebestlol@gmail.com>
2026-09-25 12:56:55 +0800

    Rewrite menu header for multi-column MegaHack layout

--- src/ui/BrokenHackMenu.hpp
+++ src/ui/BrokenHackMenu.hpp
@@ -9,11 +9,8 @@ protected:
     void keyBackClicked() override;
     void onClose(CCObject*);
 
-    // Feature toggles UI callbacks
-    void onAutoUpdate(CCObject*);
-    void onTheme(CCObject*);
-    void onInterfaceScale(CCObject*);
-    void onAnimations(CCObject*);
+    // Builds one vertical category column
+    CCNode* createColumn(std::string const& title, std::vector<std::string> const& items, float width);
 
 public:
     static BrokenHackMenu* create();
